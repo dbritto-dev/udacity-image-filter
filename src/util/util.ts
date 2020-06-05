e2423fc7d454a203ca70b682ce6bf6010e7a8303
danilobrinu <ddbn.c2@gmail.com>
2020-06-04 20:34:53 -0500

    Improve utils

--- src/util/util.ts
+++ src/util/util.ts
@@ -5,13 +5,13 @@ import Jimp from "jimp";
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
 // INPUTS
-//    inputURL: string - a publicly accessible url to an image file
+//    imageURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export const filterImageFromURL = async (inputURL: string): Promise<string> => {
-  return new Promise(async (resolve, reject) => {
+export const filterImageFromURL = async (imageURL: string): Promise<string> =>
+  new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const photo = await Jimp.read(imageURL);
       const uid = Math.floor(Math.random() * new Date().getTime())
         .toString(16)
         .replace(".", "");
@@ -28,14 +28,13 @@ export const filterImageFromURL = async (inputURL: string): Promise<string> => {
       reject(e);
     }
   });
-};
 
 // deleteLocalFiles
 // helper function to delete files on the local disk
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export const deleteLocalFiles = async (files: Array<string>) => {
+export const deleteLocalFiles = (files: Array<string>) => {
   for (let file of files) {
     try {
       fs.unlinkSync(file);
@@ -45,13 +44,13 @@ export const deleteLocalFiles = async (files: Array<string>) => {
   }
 };
 
-export const validateURL = (url: string): Boolean => {
-  if (
-    !/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
-      url
-    )
-  ) {
-    return false;
-  }
-  return true;
-};
+// validateURL
+// helper function to validate URLs
+// INPUTS
+//    url: string the url what we want to validate
+// RETURNS
+//    true or false depending on if is a valid url or not
+export const validateURL = (url: string): Boolean =>
+  /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
+    url
+  );
